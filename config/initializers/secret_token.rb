b2dda9bc538c2a9ba0c6bb68f0e0b8e523c6c78a
Sahil Bhatia <sahil@idifysolutions.com>
2012-04-03 13:31:25 +0530

    Base Code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wine::Application.config.secret_token = '09ca4bb639f509e8525ac4e3dcd37435757a6c02375a86630ad3b77c8880e5b9cbcd9ca703ae9be5899cc68081db703daf02d809c4aa5d35fa3b16612d8867d1'
